94f2e2f3685587a4d2b749e03cc1979a4640855d
user <github@dc435.org>
2022-09-10 23:06:56 -0600

    intial

---
+++ pages/en/index.js
@@ -0,0 +1,231 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+const React = require('react');
+
+const CompLibrary = require('../../core/CompLibrary.js');
+
+const MarkdownBlock = CompLibrary.MarkdownBlock; /* Used to read markdown */
+const Container = CompLibrary.Container;
+const GridBlock = CompLibrary.GridBlock;
+
+class HomeSplash extends React.Component {
+  render() {
+    const {siteConfig, language = ''} = this.props;
+    const {baseUrl, docsUrl} = siteConfig;
+    const docsPart = `${docsUrl ? `${docsUrl}/` : ''}`;
+    const langPart = `${language ? `${language}/` : ''}`;
+    const docUrl = (doc) => `${baseUrl}${docsPart}${langPart}${doc}`;
+
+    const SplashContainer = (props) => (
+      <div className="homeContainer">
+        <div className="homeSplashFade">
+          <div className="wrapper homeWrapper">{props.children}</div>
+        </div>
+      </div>
+    );
+
+    const Logo = (props) => (
+      <div className="projectLogo">
+        <img src={props.img_src} alt="Project Logo" />
+      </div>
+    );
+
+    const ProjectTitle = (props) => (
+      <h2 className="projectTitle">
+        {props.title}
+        <small>{props.tagline}</small>
+      </h2>
+    );
+
+    const PromoSection = (props) => (
+      <div className="section promoSection">
+        <div className="promoRow">
+          <div className="pluginRowBlock">{props.children}</div>
+        </div>
+      </div>
+    );
+
+    const Button = (props) => (
+      <div className="pluginWrapper buttonWrapper">
+        <a className="button" href={props.href} target={props.target}>
+          {props.children}
+        </a>
+      </div>
+    );
+
+    return (
+      <SplashContainer>
+        <Logo img_src={`${baseUrl}img/undraw_monitor.svg`} />
+        <div className="inner">
+          <ProjectTitle tagline={siteConfig.tagline} title={siteConfig.title} />
+        </div>
+      </SplashContainer>
+    );
+  }
+}
+
+class Index extends React.Component {
+  render() {
+    const {config: siteConfig, language = ''} = this.props;
+    const {baseUrl} = siteConfig;
+
+    const Block = (props) => (
+      <Container
+        padding={['bottom', 'top']}
+        id={props.id}
+        background={props.background}>
+        <GridBlock
+          align="center"
+          contents={props.children}
+          layout={props.layout}
+        />
+      </Container>
+    );
+
+    const FeatureCallout = () => (
+      <div
+        className="productShowcaseSection paddingBottom"
+        style={{textAlign: 'center'}}>
+        <a href="/blog" style={{fontSize:'3em',textDecoration:'underline'}}>
+	Upcoming Gatherings
+	
+ 	</a>
+	<br />
+	<br />
+	<br />
+	
+
+	<h2>DC435 meets every 1st Thursday of every month at 7:00PM</h2>
+        <MarkdownBlock>We meet at Bridgerland Technical College campus located at 1301 N 600 W, Logan, UT 84321 - Room 840</MarkdownBlock>
+        <h4>What is Defcon 435 or DC435</h4>
+        <p>
+	DC435 is a Defcon group of people in Cache Valley, UT who are passionate about information security. It is made up of students, professionals, researchers, and hobbyists that meet and discuss, learn, and interact with like minded people.
+DC435 is always free and open to everyone regardless of skill, age, career, gender, etc.
+If you want to present or request topics please email info(at)dc435(dot)org
+The group is community-driven, and allows its members to be active so they can learn or teach about a subject they are passionate about.
+	</p>
+	<h4>How do I become a member?</h4>
+	<p>
+	Show up! Participate, interact, learn, teach etc. That's it, welcome to DC435
+	</p>
+	<h4>Code of Conduct</h4>
+	<p>
+
+	We have NO TOLERANCE for physical/verbal/sexual harassment. If what they are doing would not be acceptable to have done to you, your best friend, your worst enemy, your sister, niece, daughter, brother, nephew, son, mother, father, or any human being, do not let them treat anyone else that way – whether you know them or not. If someone asks you to stop – stop.
+	We reserve the right to ask any individual to leave the gathering.
+	As our gathering place is donated, no illegal drug use or drinking is permitted.
+	Minors should be accompanied by their parent(s) or guardian(s).
+	</p>
+</div>
+    );
+
+    const TryOut = () => (
+      <Block id="try">
+        {[
+          {
+            content:
+              'To make your landing page more attractive, use illustrations! Check out ' +
+              '[**unDraw**](https://undraw.co/) which provides you with customizable illustrations which are free to use. ' +
+              'The illustrations you see on this page are from unDraw.',
+            image: `${baseUrl}img/undraw_code_review.svg`,
+            imageAlign: 'left',
+            title: 'Wonderful SVG Illustrations',
+          },
+        ]}
+      </Block>
+    );
+
+    const Description = () => (
+      <Block background="dark">
+        {[
+          {
+            content:
+              'This is another description of how this project is useful',
+            image: `${baseUrl}img/undraw_note_list.svg`,
+            imageAlign: 'right',
+            title: 'Description',
+          },
+        ]}
+      </Block>
+    );
+
+    const LearnHow = () => (
+      <Block background="light">
+        {[
+          {
+            content:
+              'Each new Docusaurus project has **randomly-generated** theme colors.',
+            image: `${baseUrl}img/undraw_youtube_tutorial.svg`,
+            imageAlign: 'right',
+            title: 'Randomly Generated Theme Colors',
+          },
+        ]}
+      </Block>
+    );
+
+    const Features = () => (
+      <Block layout="fourColumn">
+        {[
+          {
+            content: 'This is the content of my feature',
+            image: `${baseUrl}img/undraw_react.svg`,
+            imageAlign: 'top',
+            title: 'Upcoming Gatherings',
+          },
+          {
+            content: 'The content of my second feature',
+            image: `${baseUrl}img/undraw_operating_system.svg`,
+            imageAlign: 'top',
+            title: 'Feature Two',
+          },
+        ]}
+      </Block>
+    );
+
+    const Showcase = () => {
+      if ((siteConfig.users || []).length === 0) {
+        return null;
+      }
+
+      const showcase = siteConfig.users
+        .filter((user) => user.pinned)
+        .map((user) => (
+          <a href={user.infoLink} key={user.infoLink}>
+            <img src={user.image} alt={user.caption} title={user.caption} />
+          </a>
+        ));
+
+      const pageUrl = (page) =>
+        baseUrl + (language ? `${language}/` : '') + page;
+
+      return (
+        <div className="productShowcaseSection paddingBottom">
+          <h2>Who is Using This?</h2>
+          <p>This project is used by all these people</p>
+          <div className="logos">{showcase}</div>
+          <div className="more-users">
+            <a className="button" href={pageUrl('users.html')}>
+              More {siteConfig.title} Users
+            </a>
+          </div>
+        </div>
+      );
+    };
+
+    return (
+      <div>
+        <HomeSplash siteConfig={siteConfig} language={language} />
+        <div className="mainContainer">
+        <FeatureCallout />
+      </div>
+      </div>
+    );
+  }
+}
+
+module.exports = Index;
